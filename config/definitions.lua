02572332e4a0f22b5eb3dfdd2105cadcb71bfc42
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-01-23 12:37:57 -0600

    Character sets
    Creating the patterns that will be used for matching character types during parsing later.

---
+++ config/definitions.lua
@@ -0,0 +1,14 @@
+local definitions = {
+    -- Patterns for certain types of characters, for Lua's built-in pattern matching functions.
+
+    -- symbols: Written out manually, all symbol characters.
+    symbols = "[`%-=%[%]\\;',./!@#$%%^&*()+{}|:\"<>?]";
+
+    -- characters: All letters a-z, uppercase or lowercase, plus numbers 0-9, plus the underscore.
+    characters = "[%w_]";
+
+    -- whitespace: All whitespace characters. Space, tab, newline, and some other stuff that usually doesn't come up but is in the %s class.
+    whitespace = "%s";
+}
+
+return definitions
