dee48b074cfcb9c63bc74befa2157b6a1af76a4b
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-01-29 14:36:44 -0600

    Renaming, ordinals

--- config/tokenenum.lua
+++ config/tokenenum.lua
@@ -1,6 +1,6 @@
 -- Storing a whole string for every token would be... questionable, so here's a table that mimics an enum.
 -- String keys get their ordinal number, ordinals get the corresponding string.
--- Not all of these will be implemented. I'm just including almost everything for completeness sake because I have to update this manually if I add something new.
+-- Not all of these will be implemented. Probably still missing a bunch. Will be added... later.
 Tokens = {
     -- Operators
     "logicEQ";
@@ -89,17 +89,120 @@ Tokens = {
     "false";
 
     -- Control Keywords
-    "if";
-    "else";
+    "controlIf";
+    "controlElse";
 
-    "while";
-    "for";
-    "do";
+    "controlWhile";
+    "controlFor";
+    "controlDo";
 
-    "switch";
-    "case";
+    "controlSwitch";
+    "controlCase";
 
-    "break";
-    "continue";
-    "goto";
+    "controlBreak";
+    "controlContinue";
+    "controlGoto";
+    "controlReturn";
+
+    -- Name to Ordinal Indicies
+    logicEQ = 1;
+    logicNE = 2;
+    logicGE = 3;
+    logicLE = 4;
+    logicAND = 5;
+    logicOR = 6;
+    logicNOT = 7;
+
+    assign = 8;
+    assignAdd = 9;
+    assignSub = 10;
+    assingMul = 11;
+    assignDiv = 12;
+    assignMod = 13;
+    assignAND = 14;
+    assignOR = 15;
+    assignXOR = 16;
+    assignLSH = 17;
+    assignRSH = 18;
+
+    bitwiseAND = 19;
+    bitwiseOR = 20;
+    bitwiseXOR = 21;
+    bitwiseNOT = 22;
+    bitwiseLSH = 23;
+    bitwiseRSH = 24;
+
+    mathIncrement = 25;
+    mathDecrement = 26;
+    mathAdd = 27;
+    mathSubtract = 28;
+    mathMultiply = 29;
+    mathDivide = 30;
+    mathModulo = 31;
+
+    -- Control symbols
+    ternaryAsk = 32;
+    teraryAnswer = 33;
+
+    listSep = 34;
+    statementSep = 35;
+
+    doubleQuote = 36;
+    singleQuote = 37;
+
+    openParenthesis = 38;
+    closeParenthesis = 39;
+    openBracket = 40;
+    closeBracket = 41;
+    openCurl = 42;
+    closeCurl = 43;
+    openArrow = 44;
+    closeArrow = 45;
+
+    -- Identifiers and Literals
+    identifier = 46;
+    stringLiteral = 47;
+    numberLiteral = 48;
+
+    -- Type Keywords
+    char = 49;
+    short = 50;
+    int = 51;
+    long = 52;
+
+    float = 53;
+    double = 54;
+
+    struct = 55;
+    enum = 56;
+    union = 57;
+
+    signed = 58;
+    unsigned = 59;
+    const = 60;
+    constexpr = 70;
+    volatile = 71;
+    auto = 72;
+
+    void = 73;
+
+    bool = 74;
+    boolTrue = 75;
+    boolFalse = 76;
+
+    -- Control Keywords
+    controlIf = 77;
+    controlElse = 78;
+
+    controlWhile = 79;
+    controlFor = 80;
+    controlDo = 81;
+
+    controlSwitch = 82;
+    controlCase = 83;
+
+    controlBreak = 84;
+    controlContinue = 85;
+    controlGoto = 86;
+    controlReturn = 87;
 }
