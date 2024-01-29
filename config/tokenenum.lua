325cda9610f5125f79e277a8586feb6f59e1e649
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-01-29 14:09:20 -0600

    More enum
    still not done

--- config/tokenenum.lua
+++ config/tokenenum.lua
@@ -1,5 +1,6 @@
 -- Storing a whole string for every token would be... questionable, so here's a table that mimics an enum.
 -- String keys get their ordinal number, ordinals get the corresponding string.
+-- Not all of these will be implemented. I'm just including almost everything for completeness sake because I have to update this manually if I add something new.
 Tokens = {
     -- Operators
     "logicEQ";
@@ -61,5 +62,44 @@ Tokens = {
     "stringLiteral";
     "numberLiteral";
 
-    -- Keywords
+    -- Type Keywords
+    "char";
+    "short";
+    "int";
+    "long";
+
+    "float";
+    "double";
+
+    "struct";
+    "enum";
+    "union";
+
+    "signed";
+    "unsigned";
+    "const";
+    "constexpr";
+    "volatile";
+    "auto";
+
+    "void";
+
+    "bool";
+    "true";
+    "false";
+
+    -- Control Keywords
+    "if";
+    "else";
+
+    "while";
+    "for";
+    "do";
+
+    "switch";
+    "case";
+
+    "break";
+    "continue";
+    "goto";
 }
